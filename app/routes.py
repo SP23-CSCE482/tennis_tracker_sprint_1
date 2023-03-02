b7096fa5b6706081c86215d6b0a69070fa8a550f
Yazad <yazad_sidhwa@tamu.edu>
2023-03-02 17:05:51 -0600

    added live streaming back into main

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,27 @@
-from flask import Flask, render_template, flash, url_for, redirect, request
+from flask import Flask, render_template, flash, url_for, redirect, request, Response
 from werkzeug.utils import secure_filename
 import os
+import cv2
 from app import app
 
+camera = cv2.VideoCapture(0)  
 
+def gen_frames():  # generate frame by frame from camera
+    while True:
+      # Capture frame-by-frame
+      success, frame = camera.read()  # read the camera frame
+      if not success:
+         break
+      else:
+         ret, buffer = cv2.imencode('.jpg', frame)
+         frame = buffer.tobytes()
+         yield (b'--frame\r\n'
+                  b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+   """Video streaming home page."""
+   return render_template('index.html')
 
 @app.route('/upload')
 def upload():
@@ -20,5 +34,9 @@ def uploader():
       f.save(os.path.join(app.config['UPLOAD_FOLDER']), secure_filename(f.filename))
       return 'file uploaded successfully'
 
+@app.route('/video_feed')
+def video_feed():
+   #Video streaming route. Put this in the src attribute of an img tag
+   return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
